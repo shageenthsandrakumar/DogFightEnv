e73efdee144047bbd322af010431b593e06bac8a
Allan <76857625+allansal@users.noreply.github.com>
2023-07-05 10:39:44 -0400

    Fixed install script for macOS

--- install_as.sh
+++ install_as.sh
@@ -8,12 +8,21 @@ fi
 
 NEW_NAME="$1"
 MOD_NAME="${NEW_NAME//-/_}"
-sed -i "s/gym_env/$MOD_NAME/g" DQN_DogFight.py
-sed -i "s/gym_env/$MOD_NAME/g" ./gym-env/setup.py
-sed -i "s/gym_env/$MOD_NAME/g" ./gym-env/gym_env/__init__.py
-sed -i "s/gym_env/$MOD_NAME/g" ./gym-env/gym_env/envs/__init__.py
-sed -i "s/gym_env/$MOD_NAME/g" ./gym-env/gym_env/envs/__init__.py
 
-mv ./gym-env/gym_env "$MOD_NAME"
+if [ "$(uname)" == "Darwin" ]; then
+    sed -i '' "s/gym_env/$MOD_NAME/g" DQN_DogFight.py
+    sed -i '' "s/gym_env/$MOD_NAME/g" ./gym-env/setup.py
+    sed -i '' "s/gym_env/$MOD_NAME/g" ./gym-env/gym_env/__init__.py
+    sed -i '' "s/gym_env/$MOD_NAME/g" ./gym-env/gym_env/envs/__init__.py
+    sed -i '' "s/gym_env/$MOD_NAME/g" ./gym-env/gym_env/envs/__init__.py
+else
+    sed -i "s/gym_env/$MOD_NAME/g" DQN_DogFight.py
+    sed -i "s/gym_env/$MOD_NAME/g" ./gym-env/setup.py
+    sed -i "s/gym_env/$MOD_NAME/g" ./gym-env/gym_env/__init__.py
+    sed -i "s/gym_env/$MOD_NAME/g" ./gym-env/gym_env/envs/__init__.py
+    sed -i "s/gym_env/$MOD_NAME/g" ./gym-env/gym_env/envs/__init__.py
+fi
+
+mv ./gym-env/gym_env "./gym-env/$MOD_NAME"
 mv ./gym-env "$NEW_NAME"
 pip install -e "./$NEW_NAME"
